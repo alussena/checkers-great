70cd3d7b7d96f51e3883b77bdb5f38a6478b91a1
Alua <aluuua9@gmail.com>
2026-05-18 00:52:38 +0500

    commit

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-module.exports = nextConfig
+  typescript: {
+    ignoreBuildErrors: true, 
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: 'https://fugdhjpmseklwchecdkh.supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'sb_publishable_xE7Sju-nsPMx0kdEnA7EfA_xbsF44Y7',
+    NEXT_PUBLIC_SERVER_URL: 'https://checkers-great.onrender.com',
+  },
+};
+
+module.exports = nextConfig;
